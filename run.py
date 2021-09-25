4374b9f740a7a0553bfd019153fadbe10cb00e59
GT-Zhang <1029550448@qq.com>
2021-09-26 00:27:43 +0800

    Update.

--- run.py
+++ run.py
@@ -7,19 +7,20 @@ import os
 
 from qgui import CreateQGUI
 from qgui.bar_tools import GitHub
-from qgui.notebook_tools import ChooseFileTextButton, ChooseDirTextButton, RunButton
+from qgui.notebook_tools import ChooseFileTextButton, ChooseDirTextButton, RunButton, RadioObviousToolButton
 from qgui import MessageBox
 
 from inference import main as infer_main
 
 
 def infer(args):
-    img_path = args["文件输入框"]()
-    out_path = args["保存位置"]()
+    img_path = args["文件输入框"].get()
+    out_path = args["保存位置"].get()
+    mode = args["运行模式"].get()
 
     # 简单做个判断，保证输入是正确的
     if not os.path.exists(img_path):
-        MessageBox.info("请选择要分割的图片")
+        MessageBox.info("请选择要处理的图片")
         # 不选择就不做预测了，气！
         return 1
     if not os.path.exists(out_path):
@@ -28,11 +29,22 @@ def infer(args):
 
     # 执行操作
     print("开始制作")
-    infer_main(img_path, "paint_best.pdparams", out_path, resize_h=512, resize_w=512)
-
+    if mode == "仅图像":
+        infer_main(img_path, "paint_best.pdparams", out_path, resize_h=512, serial=True, resize_w=512)
+    elif mode == "图像->视频":
+        infer_main(img_path, "paint_best.pdparams", out_path, resize_h=512, resize_w=512, serial=True,
+                   need_animation=True)
+    else:
+        infer_main(img_path, "paint_best.pdparams", out_path, resize_h=512, resize_w=512, video=True)
     # 打开结果文件夹 不支持MAC系统
     os.startfile(out_path)
     print("处理完毕")
+
+
+def bind_dir(args):
+    # ToDo 文件选择的callback没做好
+    # img_path = args["文件输入框"].get()
+    args["保存位置"].set(os.path.dirname(args))
 
 
 # 创建主界面
@@ -44,12 +56,14 @@ main_gui.add_banner_tool(GitHub("https://github.com/AP-Kai/Paint-Master"))
 main_gui.add_notebook_tool(ChooseFileTextButton(name="文件输入框"))
 # 再加个文件夹选择工具
 main_gui.add_notebook_tool(ChooseDirTextButton(name="保存位置"))
+#
+main_gui.add_notebook_tool(RadioObviousToolButton(["仅图像", "图像->视频", "视频->视频"], name="运行模式", title="选择运行模式"))
 # 添加一个运行按钮
 main_gui.add_notebook_tool(RunButton(infer))
-main_gui.set_navigation_info("本项目基于PaddlePaddle和QGUI开发，主要功能为：图像油画化。您仅需向Robot发送一张图片，"
-                             "稍作等待便可以得到一张笔底春风、笔精墨妙、笔墨横姿、不拘绳墨、丹青不渝、丹青妙手、画中有诗、挥翰成风、"
-                             "挥毫落纸、挥洒自如、活色生香、山节藻棁、山耶云耶、涉笔成趣、铁画银钩、下笔风雷、一挥而就的一张惟妙惟肖的"
-                             "油画。并且，您可以直观的感受到AI一步步生成油画的过程，很休闲，很是解压！")
+main_gui.set_navigation_info("项目介绍", "本项目基于PaddlePaddle和QGUI开发，主要功能为：图像油画化。\n"
+                                     "稍作等待便可以得到一张笔底春风、笔精墨妙、笔墨横姿、不拘绳墨、丹青不渝、丹青妙手、画中有诗、挥翰成风、"
+                                     "挥毫落纸、挥洒自如、活色生香、山节藻棁、山耶云耶、涉笔成趣、铁画银钩、下笔风雷、一挥而就的一张惟妙惟肖的"
+                                     "油画。并且，您可以直观的感受到AI一步步生成油画的过程，很休闲，很是解压！")
 # 简单加个简介
 main_gui.set_navigation_about(author="AP-Kai",
                               version="0.0.1",
